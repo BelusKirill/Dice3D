6f4c3ba89ad8a46af0989e6035ee4509dbea8ac7
BelusKirill <belus@vikab.by>
2023-04-20 22:13:36 +0300

    animate panel seppring

--- Assets/Scripts/UI/ControllerPanelCanter.cs
+++ Assets/Scripts/UI/ControllerPanelCanter.cs
@@ -5,26 +5,71 @@ using UnityEngine;
 public class ControllerPanelCanter : MonoBehaviour
 {
     [Header("Анимация вернего бара")]
-    public Animator anim;
+    public Animator animPanelDices;
+    public Animator animPanelSetting;
 
-    private bool _states = true;
+    private bool _DicesStates = true;
+    private bool _SettingStates = true;
 
     public void ChangingState()
     {
-        if (_states)
+        if (_DicesStates)
         {
-            anim.SetTrigger("Open");
-            _states = !_states;
+            animPanelDices.SetTrigger("Open");
+            _DicesStates = !_DicesStates;
         }
         else
         {
-            anim.SetTrigger("Close");
-            _states = !_states;
+            animPanelDices.SetTrigger("Close");
+            _DicesStates = !_DicesStates;
         }
     }
 
-    public void Test()
+    public void PressDicesBtn()
     {
-        Debug.Log("TTT");
+        if (!_SettingStates)
+        {
+            Setting();
+        }
+
+        Dices();
+    }
+
+    public void PressSettingBtn()
+    {
+        if (!_DicesStates)
+        {
+            Dices();
+        }
+        
+        Setting();
+    }
+
+    private void Dices()
+    {
+        if (_DicesStates)
+        {
+            animPanelDices.SetTrigger("Open");
+            _DicesStates = !_DicesStates;
+        }
+        else
+        {
+            animPanelDices.SetTrigger("Close");
+            _DicesStates = !_DicesStates;
+        }
+    }
+
+    private void Setting()
+    {
+        if (_SettingStates)
+        {
+            animPanelSetting.SetTrigger("Open");
+            _SettingStates = !_SettingStates;
+        }
+        else
+        {
+            animPanelSetting.SetTrigger("Close");
+            _SettingStates = !_SettingStates;
+        }
     }
 }
